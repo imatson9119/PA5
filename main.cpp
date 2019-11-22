ef1485d8dbf80b65facf660dc478949e9ef99ca8
imatson9119 <imatson9119@tamu.edu>
2019-11-21 18:05:31 -0600

    Completed main method

--- main.cpp
+++ main.cpp
@@ -6,39 +6,32 @@
 
 using namespace std;
 int main(){
-
-	int n = 10000;
-	const int numPoints = 99;
-	const int numTrials = 100;
-	double start = .01;
-	double data[numPoints] = { 0 };
-	for (int trial = 1; trial <= numTrials; trial++) {
-		cout << "Starting trial " << trial << endl;
-		for (int i = 0; i < numPoints; i++) {
-			if (i % 10 == 0) {
-				cout << "\tTrial #" << i << endl;
-			}
-			ProbingHashTable ht;
-			ht.setLoadFactor(start + (1 - start) / numPoints * i);
-			StopWatch t;
-			ifstream file("dictionary.txt");
-			t.start();
-			for (int j = 0; j < n; j++) {
-				string word;
-				file >> word;
-				if (word != "")
-					ht.insert(word, 1);
-			}
-			t.stop();
-			data[i] += t.seconds() / numTrials;
+	////////////////////////////////////////////////////////////
+	//Default code
+	////////////////////////////////////////////////////////////
+	cout << "Please enter the name of the input file (including extension): ";
+	string fileName;
+	cin >> fileName;
+	DoubleHashTable dht;
+	ChainingHashTable cht;
+	ProbingHashTable pht;
+	ifstream file(fileName);
+	while(!file.eof()){
+		string word;
+		file >> word;
+		if (word != "") {
+			dht.insert(word, 1);
+			cht.insert(word, 1);
+			pht.insert(word, 1);
 		}
 	}
-	cout << endl;
-	for (int i = 0; i < numPoints; i++) {
-		cout << data[i] << endl;
-	}
-
-
+	pht.printAll("Probing.txt");
+	cht.printAll("Chaining.txt");
+	dht.printAll("DoubleHashing.txt");
+	
+	////////////////////////////////////////////////////////////
+	//Code for testing variable insertions
+	////////////////////////////////////////////////////////////
 	/*
 	int n = 1000000;
 	const int numPoints = 100;
@@ -69,14 +62,40 @@ int main(){
 		cout << data[i] << endl;
 	}
 	*/
+
+
+	////////////////////////////////////////////////////////////
+	//Code for testing variable load factor
+	////////////////////////////////////////////////////////////
 	/*
-	ifstream file("dictionary.txt");
-	while(!file.eof()){
-		string word;
-		file >> word;
-		if(word != "")
-			ht.insert(word, 1);
+	int n = 10000;
+	const int numPoints = 99;
+	const int numTrials = 100;
+	double start = .01;
+	double data[numPoints] = { 0 };
+	for (int trial = 1; trial <= numTrials; trial++) {
+		cout << "Starting trial " << trial << endl;
+		for (int i = 0; i < numPoints; i++) {
+			if (i % 10 == 0) {
+				cout << "\tTrial #" << i << endl;
+			}
+			ProbingHashTable ht;
+			ht.setLoadFactor(start + (1 - start) / numPoints * i);
+			StopWatch t;
+			ifstream file("dictionary.txt");
+			t.start();
+			for (int j = 0; j < n; j++) {
+				string word;
+				file >> word;
+				if (word != "")
+					ht.insert(word, 1);
+			}
+			t.stop();
+			data[i] += t.seconds() / numTrials;
+		}
 	}
-	ht.printAll("outFile1.txt");
-	*/
+	cout << endl;
+	for (int i = 0; i < numPoints; i++) {
+		cout << data[i] << endl;
+	}*/
 }
